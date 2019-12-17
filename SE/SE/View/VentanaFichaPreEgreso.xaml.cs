aaae56ba68063fae38e4e64341ce638267872432
CesarDiaz2712 <cesar_diaz16@outlook.com>
2019-12-16 18:22:47 -0600

    FichaPreEgreso resgistro

--- SE/SE/View/VentanaFichaPreEgreso.xaml.cs
+++ SE/SE/View/VentanaFichaPreEgreso.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using SE.model.dao;
 using SE.model.pocos;
 
 namespace SE.View
@@ -21,15 +22,50 @@ namespace SE.View
     public partial class VentanaFichaPreEgreso : Window
     {
         private FichaPreEgreso ficha;
+        private Boolean resultado;
         public VentanaFichaPreEgreso(FichaPreEgreso ficha)
         {
             this.ficha = ficha;
             InitializeComponent();
         }
 
-        private void Btn_Guardar_Click(object sender, RoutedEventArgs e)
+        public bool Resultado { get => resultado; set => resultado = value; }
+        private void btn_Guardar_Click(object sender, RoutedEventArgs e)
         {
+            string sexo = "";
+            string nacionalidad = "";
+            if (rbHombre.IsChecked==true)
+            {
+                sexo = "Hombre";
+            }
+            else
+            {
+                sexo = "Mujer";
+            }
 
+            if (rbMexicana.IsChecked==true)
+            {
+                nacionalidad = "Mexicana";
+            }
+            else
+            {
+                nacionalidad = txtNacionalidad.Text;
+            }
+
+            this.ficha.Sexo = sexo;
+            this.ficha.Nacionalidad = nacionalidad;
+            this.ficha.Telefono = txtTelefono.Text;
+            this.ficha.Calle = txtCalle.Text;
+            this.ficha.Email = txtEmail.Text;
+            this.ficha.NumeroCasa = txtNumeroCasa.Text;
+            this.ficha.Colonia = txtColonia.Text;
+            this.ficha.Ciudad = txtCiudad.Text;
+            this.ficha.Estado = txtEstado.Text;
+            this.ficha.CodigoPostal = txtCP.Text;
+            this.resultado = FichaPreEgresoDAO.guardar(this.ficha);
+            this.Close();
         }
-    }
-}
+     }
+    
+ }
+
